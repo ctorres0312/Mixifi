748d482791935ea0e6027b0bee8b211a79c7a04e
ctorres0312 <113136320+ctorres0312@users.noreply.github.com>
2023-10-22 21:39:10 -0400

    added footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,15 +30,17 @@ addEventOnElem(navLinks, "click", closeNavbar);
 
 
 
-// header active when scroll down to 100px
+// header & back top btn active when scroll down to 100px
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]")
 
 const headerActive = function () {
     if (window.scrollY > 100) {
-        header.classList.add("active")
+        backTopBtn.classList.add("active")
     } else {
         header.classList.remove("active");
+       backTopBtn.classList.remove("active");
     }
 }
 
