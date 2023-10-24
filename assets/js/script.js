153b816729c8e4f0864bd552aaaa78beb2949adc
ctorres0312 <113136320+ctorres0312@users.noreply.github.com>
2023-10-24 02:39:05 -0400

    final v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,7 @@ const backTopBtn = document.querySelector("[data-back-top-btn]")
 
 const headerActive = function () {
     if (window.scrollY > 100) {
+        header.classList.add("active")
         backTopBtn.classList.add("active")
     } else {
         header.classList.remove("active");
